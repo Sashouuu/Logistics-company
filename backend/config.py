2195631a939aa5fb17c2f8be62eaed4d83a4aee7
Sashouuu <alex.delev@delev.com>
2026-01-19 15:52:58 +0200

    some code

---
+++ backend/config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://{user}:{pwd}@{host}:{port}/{db}?charset=utf8mb4"
+    ).format(
+        user=os.getenv("DB_USER", "root"),
+        pwd=os.getenv("DB_PASSWORD", ""),
+        host=os.getenv("DB_HOST", "127.0.0.1"),
+        port=os.getenv("DB_PORT", "3306"),
+        db=os.getenv("DB_NAME", "logistics_company"),
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "change-me")
